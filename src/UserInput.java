72adb934ad362b799b658b97d551fa7599c456cd
Rohit Kumawat <rohitkumawat296@gmail.com>
2024-12-12 10:07:34 +0530

    Third

---
+++ src/UserInput.java
@@ -0,0 +1,166 @@
+//45+7
+
+/*public class UserInput {
+    public static void main(String[]args){
+        int a=45,b=7;
+        int c=a+b;
+        System.out.println("value of c "+c);
+    }
+}*/
+
+//234-55
+
+/*public class UserInput {
+    public static void main(String[]args){
+        int a=234,b=55;
+        int c=a-b;
+        System.out.println("the value of c "+c);
+    }
+}*/
+
+//56*4
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        int a = 56, b = 4;
+        int c = a * b;
+        System.out.println("the value of c " + c);
+    }
+}*/
+
+//22/2
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        int a = 22, b = 2;
+        int c = a * b;
+        System.out.println("the value of c " + c);
+    }
+}*/
+
+//45+67
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        int a = 45, b = 67;
+        int c = a + b;
+        System.out.println("the value of c " + c);
+    }
+}*/
+
+//23-2
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        int a=23,b=2;
+        int c=a-b;
+        System.out.println("the value of c "+c);
+    }
+}*/
+
+//34*5
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        int a=34,b=5;
+        int c=a*b;
+        System.out.println("the value of c "+c);
+    }
+}*/
+
+//34+67+23+12+6
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        int a=34,b=67,c=23,d=12,e=6;
+        int f=a+b+c+d+e;
+        System.out.println("the value of f "+f);
+    }
+}*/
+
+//23+89+12+56
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        int a=23,b=89,c=12,d=56;
+        int e=a+b+c+d;
+        System.out.println("the value of e"+e);
+    }
+}*/
+
+//56*34+12/2-13
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        int a=56,b=34,c=12,d=2,e=13;
+        int f=a*b+c/d-e;
+        System.out.println("the value of f "+f);
+    }
+}*/
+
+//1.2*6.7-4.5/2
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        float a=1.2f,b=6.7f,c=4.5f,d=2;
+        float e=a*b-c/d;
+        System.out.println("the value of e "+e);
+    }
+}*/
+
+//1.6*(12-2)/2+7.8-1.5
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        float a=1.6f,b=12,c=2,d=2,e=7.8f,f=1.5f;
+        float result=a*(b-c)/d+e-f;
+        System.out.println("the value of result "+result);
+    }
+}*/
+
+//4.5*1.2-(82/2)-5.6 +1.234
+
+/*public class UserInput {
+    public static void main(String[]args){
+        float a=4.5f,b=1.2f,c=82,d=2,e=5.6f,f=1.234f;
+        float result=a*b-(c/d)-e+f;
+        System.out.println("the the value of result " +result);
+    }
+}*/
+
+//8*1+(4-2)/1+6
+
+/*public class UserInput {
+    public static void main(String[]args){
+        int a=8,b=1,c=4,d=2,e=1,f=6;
+        int g=a*b+(c-d)/e+f;
+        System.out.println("the value of g "+g);
+    }
+}*/
+
+//3.4+1.2*(3.7-1.2)/2+6.8
+
+/*public class UserInput {
+    public static void main(String[]args){
+        float a=3.4f,b=1.2f,c=3.7f,d=1.2f,e=2,g=6.8f;
+        float result=a+b*(c-d)/e+g;
+        System.out.println("the value of result "+result);
+    }
+}*/
+
+//8.9*1.67-(6.21-5.6)+7.8-34 +157
+
+/*public class UserInput {
+    public static void main(String[] args) {
+        float a=8.9f,b=1.67f,c=6.21f,d=5.6f,e=7.8f,f=34,g=157;
+        float reslut=a*b-(c-d)+e-f+g;
+        System.out.println("the value of result "+reslut);
+    }
+}*/
+
+
+
+
+
+
+
