4ffe77cf71fd953edbca1b700d270adeb758348b
Rohit Kumawat <rohitkumawat296@gmail.com>
2024-12-12 10:07:34 +0530

    second

---
+++ src/A.java
@@ -0,0 +1,44 @@
+/*hello world
+
+public class A {
+    public static void main(String[]args){
+        System.out.println("hello world");
+    }
+}*/
+
+/*five friends
+
+public class A {
+    public static void main(String[]args){
+        System.out.print("Ravi");
+        System.out.print("Aman");
+        System.out.print("Preetak");
+        System.out.print("Isha");
+        System.out.print("Senha");
+    }
+}*/
+
+/*five fav food name :new line
+
+public class A {
+    public static void main(String[]args){
+        System.out.println("Dal");
+        System.out.println("Rice");
+        System.out.println("Roti");
+        System.out.println("Ghewar");
+        System.out.println("Kachori");
+    }
+}*/
+
+/*five city name :new line
+
+public class A {
+    public static void main(String[]args){
+        System.out.println("Lucknow");
+        System.out.println("Chennai");
+        System.out.println("Delhi");
+        System.out.println("Surat");
+        System.out.println("Agra");
+
+    }
+}*/
